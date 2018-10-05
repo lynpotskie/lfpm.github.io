5ed7ffb227ae00d47600006b3d0e26797fabf9bd
lynpotskie <lynpot_77@yahoo.com>
2018-10-04 19:25:14 -0600

    first commit

---
+++ public/script.js
@@ -0,0 +1,24 @@
+
+window.onload = function() {
+
+    var button = document.querySelector('button');
+  
+    var button2 = document.querySelector('.other');
+  
+  
+  
+    button.addEventListener('click', function() {
+  
+      alert("Say Something !!!");
+  
+    });
+  
+    
+  
+    button2 .addEventListener('click', function() {
+  
+      alert("Say it now or Never!!!!!");
+  
+    });
+  
+  }
